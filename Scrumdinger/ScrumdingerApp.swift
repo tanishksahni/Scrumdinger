63284de79e242419f32bed8a2b06d6fbdf8381c2
tanishksahni <tanishk.sahni2002@gmail.com>
2024-03-02 11:17:33 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Tanishk Sahni on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
